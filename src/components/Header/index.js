b86453d7387937100cfcb4c9d956c4c3d89e644f
David Mendes <davidgmendes@hotmail.com>
2022-05-26 18:52:23 -0300

    Colinha da aula com cookies

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,10 @@ import {Container} from './styles.js'
 import Logo from '../../assets/unblogo.png'
 import { Link } from 'react-router-dom'
 import { useUserContext } from '../../context/useUserContext.js'
+import { FiLogOut } from 'react-icons/fi'
 
 const Header = () => {
-    const {user} = useUserContext()
+    const {user, logout} = useUserContext()
     return(
         <Container>
             <Link to="/">
@@ -13,7 +14,12 @@ const Header = () => {
             <Link to="/">Homepage</Link>
             <Link to="/estudantes">Alunos</Link>
             <Link to="/turmas">Turmas</Link>
-            { user ? user.email : <Link to="/login">Logar</Link> }
+            { user ? 
+            <>
+                {user.email}
+                <FiLogOut style={{"cursor": "pointer"}} onClick={logout}/>
+            </>
+            : <Link to="/login">Logar</Link> }
         </Container>
     )
 }
--- src/context/useUserContext.js
+++ src/context/useUserContext.js
@@ -1,5 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
+import Cookie from "js-cookie"
 
 const UserContext = createContext({}); 
 
@@ -14,6 +15,7 @@ const UserProvider = ({children}) => {
                 setUser(response.data)
                 api.defaults.headers.common['X-Admin-Email'] = response.data.email
                 api.defaults.headers.common['X-Admin-Token'] = response.data.authentication_token
+                Cookie.set('sigaav2.user', JSON.stringify(response.data), {expires: 1})
                 alert("Usuário logado!")
             }
         } catch(e){
@@ -21,8 +23,22 @@ const UserProvider = ({children}) => {
         }
     }
 
+    const logout = async () => {
+        if(window.confirm("Deseja realmente sair de sua conta?")){
+            setUser(undefined);
+            Cookie.remove('sigaav2.user')
+        }
+    }
+
+    useEffect(() => {
+        const retrievedUser = Cookie.get('sigaav2.user');
+        if(retrievedUser){
+            setUser(JSON.parse(retrievedUser))
+        }
+    }, [])
+
     return (
-        <UserContext.Provider value={{login, user}}>
+        <UserContext.Provider value={{login, user, logout}}>
             {children}
         </UserContext.Provider>
     )
